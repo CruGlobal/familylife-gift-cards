dc0a8dea9aabcb4e23f0a9b8c3a936e53a6166e1
Andrew Roth <andrewroth@gmail.com>
2024-12-09 11:01:27 -0500

    add stage to production hosts to see if that makes it load

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,8 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
-    "familylife-gift-cards.cru.org"
+    "familylife-gift-cards.cru.org",
+    "familylife-gift-cards-stage.cru.org"
   ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
