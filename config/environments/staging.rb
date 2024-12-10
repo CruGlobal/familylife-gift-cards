f9619f640fdfb509551b1421b7588e71b4308386
Andrew Roth <andrewroth@gmail.com>
2024-12-09 23:59:10 -0500

    allow 10.* ips

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -97,7 +97,8 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
-    "familylife-gift-cards-stage.cru.org"
+    "familylife-gift-cards-stage.cru.org",
+    IPAddr.new("10.0.0.0/8"),
   ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
