8cbd5b5c32bf32cbf6fcf2eac19418120c818b1c
Andrew Roth <andrewroth@gmail.com>
2024-12-10 11:49:00 -0500

    try to allow monitors/lb without host_authorization

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -100,6 +100,7 @@ Rails.application.configure do
     "familylife-gift-cards-stage.cru.org",
     IPAddr.new("10.0.0.0/8"),
   ]
+
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/monitors/lb" } }
 end
