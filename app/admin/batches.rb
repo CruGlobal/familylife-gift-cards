fd21689ee09e0bbce5a9b940e1b3a61111ec61d8
Andrew Roth <andrewroth@gmail.com>
2025-01-20 13:10:33 -0500

    try to get some sort of helper class workign for AA

--- app/admin/batches.rb
+++ app/admin/batches.rb
@@ -55,12 +55,4 @@ ActiveAdmin.register Batch do
 
     f.actions
   end
-
-  def reload_js_based_on_gift_card_type
-    %|var params = new URLSearchParams(location.search); params.set('gift_card_type', $('#batch_gift_card_type').val()); window.location.search = params.toString();|
-  end
-
-  def reload_js_hint
-    "Changing this value will reload the form to ensure the correct fields are present."
-  end
 end
