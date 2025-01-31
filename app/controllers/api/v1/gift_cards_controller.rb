55ace7562a72a1bb26d4be9ac8cb66b900b992e8
Andrew Roth <andrewroth@gmail.com>
2025-01-31 14:04:30 -0500

    more tests on api, including updating registrations count

--- app/controllers/api/v1/gift_cards_controller.rb
+++ app/controllers/api/v1/gift_cards_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::GiftCardsController < Api::V1::ApplicationController
-  def show
-    gift_card = GiftCard.find_by(certificate: params[:id])
+  before_action :load_gift_card_from_certificate
 
-    render json: gift_card
+  def show
+    @gift_card = GiftCard.find_by(certificate: params[:id])
+
+    render json: @gift_card
   end
 
   # an update automatically means to use up one registration
   def update
-    gift_card = GiftCard.find_by(certificate: params[:id])
-
-    if gift_card.registrations_available.to_i <= 0
+    if @gift_card.registrations_available.to_i <= 1
       render json: {
         error: "No registrations available",
         status: 403
@@ -17,7 +17,20 @@ class Api::V1::GiftCardsController < Api::V1::ApplicationController
       return
     end
 
-    gift_card.update(registrations_available: gift_card.registrations_available - 1)
-    render json: gift_card
+    @gift_card.update(registrations_available: @gift_card.registrations_available - 2)
+    render json: @gift_card
+  end
+
+  private
+
+  def load_gift_card_from_certificate
+    @gift_card = GiftCard.find_by(certificate: params[:id])
+
+    unless @gift_card
+      render json: {
+        error: "No gift card found by that certificate",
+        status: 404
+      }, status: 404
+    end
   end
 end
---
+++ spec/controllers/api/v1/gift_cards_controller_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Api::V1::GiftCardsController do
+  let(:api_key) { create(:api_key) }
+  let!(:gift_card) { create(:gift_card, certificate: "cert") }
+
+  before do
+    request.env["HTTP_ACCEPT"] = "application/json"
+  end
+
+  context "#show" do
+    it "returns gift card info" do
+      get :show, params: {access_token: api_key.access_token, id: gift_card.certificate, format: :json}
+      expect(JSON.parse(response.body)).to eq(JSON.parse(gift_card.to_json))
+    end
+    
+    it "handles gift card not found" do
+      get :show, params: {access_token: api_key.access_token, id: "not existing", format: :json}
+      expect(response).to have_http_status(404)
+    end
+  end
+
+  context "#update" do
+    it "handles gift card not found" do
+      put :update, params: {access_token: api_key.access_token, id: "not existing", format: :json}
+      expect(response).to have_http_status(404)
+    end
+
+    it "handles a gift card that doesn't have any more registrations available" do
+      gift_card.update(registrations_available: 1)
+      put :update, params: {access_token: api_key.access_token, id: gift_card.certificate, format: :json}
+      expect(response).to have_http_status(403)
+    end
+
+    it "subtracts 2 from registrations_available" do
+      gift_card.update(registrations_available: 10)
+      put :update, params: {access_token: api_key.access_token, id: gift_card.certificate, format: :json}
+      gift_card.reload
+      expect(JSON.parse(response.body)).to eq(JSON.parse(gift_card.to_json))
+      expect(JSON.parse(response.body)["registrations_available"]).to eq(8)
+      expect(gift_card.registrations_available).to eq(8)
+    end
+  end
+end
