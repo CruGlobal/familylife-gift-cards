833915d3ee001fe1277d21f18a1f83a06d79b6ba
David Hollenberger <david.hollenberger@cru.org>
2025-07-31 16:49:44 -0400

    Skip database connection in staging for load balancer health checks

--- app/controllers/monitors_controller.rb
+++ app/controllers/monitors_controller.rb
@@ -2,7 +2,7 @@ class MonitorsController < ApplicationController
   # skip_before_action :require_login, raise: false
 
   def lb
-    User.first
+    User.first unless Rails.env.staging?
     render plain: "OK"
   end
 end
